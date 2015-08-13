50fbdb510d1013b0904ae801d38734e0f0ad65fd
faiq <faiqrazarizvi@gmail.com>
2015-08-13 15:04:59 -0400

    adds additional scripts and uses idiomatic way to set default props

--- app.js
+++ app.js
@@ -3,50 +3,29 @@ import React from 'react'
 class EmailList extends React.Component {
   constructor () {
     super()
-    this.props = {
-      name: 'Email List'
-    }
   }
   render () { 
-    var Emails = [<Email></Email>]
+    var Emails = [<Email/>,<Email/>,<Email/>]
     return <div className="Emails">
-      <h1> {this.props.name} </h1> 
-      {Emails} 
+      <h1> {this.props.name} </h1>
+      <ul>
+        {Emails}
+      </ul>
     </div>
   }
+
 }
 
 class Email extends React.Component {
-  constructor () {
-    super()
-    this.props = {
-      snippet: 'hey wassup hello'
-    }
+  constructor (props) {
+    super(props)
   }
   render () {
     console.log(JSON.stringify(this.props))
-    return <div>
-      <h1> {this.props.snippet} </h1>
-    </div>
-  } 
-}
-
-React.render(<Email snippet="hi"/>, document.querySelector('#content'))
-/*
-class App extends React.Component {
-  constructor () {
-    super()
-    this.state = { n: 0 }
-  }
-  render () {
-    return <div>
-      <h1>clicked {this.state.n} times</h1>
-      <button onClick={this.handleClick.bind(this)}>click me!</button>
-    </div>
-  }
-  handleClick () {
-    this.setState({ n: this.state.n + 1 })
+    return <li> 
+      {this.props.snippet} 
+    </li>
   }
 }
-React.render(<App />, document.querySelector('#content'))
-*/
+Email.defaultProps = { snippet: 'default message'}
+React.render(<EmailList name="not read"/>, document.querySelector('#content'))
