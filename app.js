4aafa3bd8f670db53402b6120b74697838a88d62
faiq <faiqrazarizvi@gmail.com>
2015-08-12 23:05:35 -0400

    something is wrong with the sample app

---
+++ app.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+/*class EmailList extends React.Component {
+  constructor () {
+    super()
+    this.props = {
+      name: 'Email List'
+    }
+  }
+  render () { 
+    var Emails = [<Email></Email>]
+    return <div className="Emails">
+      <h1> {this.props.name} </h1> 
+      {Emails} 
+    </div>
+  }
+}
+
+class Email extends React.Component {
+  constructor () {
+    super()
+    this.props = {
+      snippet: 'hey wassup hello'
+    }
+  }
+  render () {
+    return <div>
+      <b> {this.props.snippet} </b>
+    </div>
+  } 
+}
+
+React.render(<Email />, document.querySelector('#content'))
+*/
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = { n: 0 }
+  }
+  render () {
+    return <div>
+      <h1>clicked {this.state.n} times</h1>
+      <button onClick={this.handleClick.bind(this)}>click me!</button>
+    </div>
+  }
+  handleClick () {
+    this.setState({ n: this.state.n + 1 })
+  }
+}
+React.render(<App />, document.querySelector('#content'))
