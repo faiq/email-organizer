6b66826041894eef41bbe98a34a5f166fcd82480
faiq <faiqrazarizvi@gmail.com>
2015-08-14 18:10:47 -0400

    styling changes and adds a gemfile for sass

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import $ from 'jquery'
 
 class EmailList extends React.Component {
   constructor () {
@@ -8,7 +9,7 @@ class EmailList extends React.Component {
     var Emails = [<Email/>,<Email/>,<Email/>]
     return <div className="Emails">
       <h1> {this.props.name} </h1>
-      <ul>
+      <ul id="EmailList" onDragOver={this.dragOver}>
         {Emails}
       </ul>
     </div>
@@ -23,9 +24,17 @@ class Email extends React.Component {
   render () {
     console.log(JSON.stringify(this.props))
     return <li> 
-      <div className="MyComponent" draggable="true"> <header> {this.props.title} </header>  {this.props.snippet} </div>
+      <div className="Email" draggable="true"> 
+        <div className="header"> 
+          <div id="from"> {this.props.from} </div>  
+          <div id="date"> {this.props.date} </div>
+        </div>  
+        <div id="snippet">
+          {this.props.snippet} 
+        </div>
+      </div>
     </li>
   }
 }
-Email.defaultProps = { snippet: 'default message', title: 'default title'}
+Email.defaultProps = { snippet: 'you got a job!', from: 'Chris Wiggins', date: '11/24/2015'}
 React.render(<EmailList name="not read"/>, document.querySelector('#content'))
