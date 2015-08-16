091d133a656ceaaa00dc7a403788ee40f2200325
faiq <faiqrazarizvi@gmail.com>
2015-08-16 16:59:51 -0400

    gets basic example of drag and drop working.

--- public/components/Email.js
+++ public/components/Email.js
@@ -3,12 +3,27 @@ import { DragSource } from 'react-dnd'
 
 const emailSource = {
   beginDrag(props) {
+    console.log('this is props ' + JSON.stringify(props))
     return {
-      from: props.from
+      from: props.from,
+      snippet: props.snippet,
+      date: props.date 
+    }
+  },
+  endDrag(props, monitor) {
+    let item = monitor.getItem();
+    let dropResult = monitor.getDropResult();
+
+    dropResult = JSON.stringify(dropResult)
+    if (dropResult) {
+      window.alert(`You dropped ${item} into ${dropResult}!`);
     }
   }
 }
 
+/**
+ * Specifies which props to inject into your component.
+ */
 function collect(connect, monitor) {
   return {
     connectDragSource: connect.dragSource(),
@@ -19,18 +34,17 @@ function collect(connect, monitor) {
 class Email extends React.Component {
   constructor (props) {
     super(props)
-  
   }
   render () {
-    const { connectDragSource } = this.props
-    return connectDragSource (<li> 
+    const { connectDragSource, isDragging, from, date, snippet } = this.props
+    return connectDragSource (<li>
       <div className="Email" draggable="true"> 
         <div className="header"> 
-          <div id="from"> {this.props.from} </div>
-          <div id="date"> {this.props.date} </div>
+          <div id="from"> {from} </div>
+          <div id="date"> {date} </div>
         </div>  
         <div id="snippet">
-          {this.props.snippet} 
+          {snippet} 
         </div>
       </div>
     </li>
--- public/components/EmailList.js
+++ public/components/EmailList.js
@@ -2,29 +2,53 @@ import React from 'react'
 import { DropTarget } from 'react-dnd';
 import Email from './Email'
 
+const EmailTarget = {
+  drop(props) {
+    return { name: props.name }
+  }
+}
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  }
+}
+
 class EmailList extends React.Component {
   constructor () {
     super()
   }
+
+   renderOverlay(color) {
+    return (
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: '100%',
+        width: '100%',
+        zIndex: 1,
+        opacity: 0.5,
+        backgroundColor: color,
+      }} />
+    )
+  }
+
   render () { 
-    var Emails = [<Email />,<Email />,<Email />]
-    return <div className="Emails">
-      <h1> {this.props.name} </h1>
-      <ul id="EmailList">
-        {Emails}
-      </ul>
-    </div>
+    const { connectDropTarget, isDragging, isOver, name } = this.props
+    var Emails = [<Email snippet="hi" />,<Email snippet="hi" />,<Email snippet="hi" />]
+    return connectDropTarget(
+      <div className="Emails">
+        <h1> {name} </h1>
+        <ul id="EmailList">
+          {Emails}
+          {isOver && this.renderOverlay('green')}
+        </ul>
+      </div>
+    )
   }
 }
 
-const EmailTarget = {
-  drop(props, monitor) {
-    props.onDrop(monitor.getItem())
-  }
-}
-
-export default DropTarget('Email', EmailTarget, (connect, monitor) => ({
-  connectDropTarget: connect.dropTarget(),
-  isOver: monitor.isOver(),
-  canDrop: monitor.canDrop()
-}))(EmailList)
+export default DropTarget('Email', EmailTarget, collect)(EmailList)
